7bef027c103c939e34b84766ea2e9b25d27b1f9b
Wolf McNally <wolf@wolfmcnally.com>
2023-12-01 17:45:21 -0800

    Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.9
 
 import PackageDescription
 
@@ -9,7 +8,6 @@ let package = Package(
         .iOS(.v15), .macOS(.v12), .tvOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "WolfSwiftUI",
             targets: ["WolfSwiftUI"]),
@@ -17,8 +15,6 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WolfSwiftUI",
             dependencies: [])
